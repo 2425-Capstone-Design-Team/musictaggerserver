76d744aeaa2445ac469258f1faa0f8a83daa97a6
Dufrane-S <sskim629@gmail.com>
2023-04-23 14:04:56 +0900

    메타데이터

--- src/main/java/com/tftf/musictaggerserver/MetadataController.java
+++ src/main/java/com/tftf/musictaggerserver/MetadataController.java
@@ -3,10 +3,25 @@ package com.tftf.musictaggerserver;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.AudioHeader;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -86,4 +101,146 @@ public class MetadataController {
 
         return null;
     }
+    @RequestMapping("/readmeta")
+    public String readmeta() throws IOException {
+
+        String DATA_DIRECTORY = "C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\";
+        File dir = new File(DATA_DIRECTORY);
+        String[] filenames = dir.list((f,name)->name.endsWith(".mp3"));
+        String result="";
+        for (String filename : filenames) {
+            System.out.println(filename);
+            result=result+filename;
+        }
+        File metadata = new File("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\meta\\meta.txt");
+        if(metadata.exists()){
+            try {
+                BufferedReader reader = new BufferedReader(
+                        new FileReader("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\meta\\meta.txt")                );
+                String str;
+                while ((str = reader.readLine()) != null) {
+                    System.out.println(str);
+                }
+                reader.close();
+                if (!(result==str)){
+                    Path metafile = Paths.get("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\meta\\meta.txt");
+                    Files.deleteIfExists(metafile);
+                    try{
+                        BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\meta\\meta.txt",true));
+                        writer.write(result);
+                        writer.flush();
+                        writer.close();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    //todo json 업데이트
+                    Path metajsonfile = Paths.get("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\meta\\meta.json");
+                    Files.deleteIfExists(metajsonfile);
+                    JSONObject listmp3meta = new JSONObject();
+                    JSONArray meta_array = new JSONArray();
+                    for(String filename: filenames){
+                        meta_array.add(getMeta(filename));
+                    }
+                    listmp3meta.put("list_array",meta_array);
+                    try {
+                        FileWriter file = new FileWriter("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\meta\\meta.json");
+                        file.write(listmp3meta.toJSONString());
+                        file.flush();
+                        file.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }else{
+            Path metafile = Paths.get("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\meta\\meta.txt");
+            Files.deleteIfExists(metafile);
+            try{
+                BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\meta\\meta.txt",true));
+                writer.write(result);
+                writer.flush();
+                writer.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            JSONObject listmp3meta = new JSONObject();
+            JSONArray meta_array = new JSONArray();
+            for(String filename: filenames){
+                meta_array.add(getMeta(filename));
+            }
+            listmp3meta.put("list_array",meta_array);
+            try {
+                FileWriter file = new FileWriter("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\meta\\meta.json");
+                file.write(listmp3meta.toJSONString());
+                file.flush();
+                file.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+
+        File file = new File("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\1000.mp3");
+        AudioFile f;
+
+        {
+            try {
+                f = AudioFileIO.read(file);
+            } catch (CannotReadException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (TagException e) {
+                throw new RuntimeException(e);
+            } catch (ReadOnlyFileException e) {
+                throw new RuntimeException(e);
+            } catch (InvalidAudioFrameException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        Tag tag = f.getTag();
+        AudioHeader audioHeader = f.getAudioHeader();
+        return tag.getFirst(FieldKey.ARTIST)+" "+tag.getFirst(FieldKey.ALBUM)+" "+tag.getFirst(FieldKey.TITLE)+" "+audioHeader.getTrackLength()+ tag.getFirst(FieldKey.COVER_ART);
+    }
+    public JSONObject getMeta(String filename)
+    {
+        JSONObject mp3meta = new JSONObject();
+
+        File file = new File("C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\"+ filename);
+        AudioFile f;
+
+        {
+            try {
+                f = AudioFileIO.read(file);
+            } catch (CannotReadException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (TagException e) {
+                throw new RuntimeException(e);
+            } catch (ReadOnlyFileException e) {
+                throw new RuntimeException(e);
+            } catch (InvalidAudioFrameException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        Tag tag = f.getTag();
+        AudioHeader audioHeader = f.getAudioHeader();
+        mp3meta.put("Artist",tag.getFirst(FieldKey.ARTIST));
+        mp3meta.put("Album",tag.getFirst(FieldKey.ALBUM));
+        mp3meta.put("Title",tag.getFirst(FieldKey.TITLE));
+        mp3meta.put("TrackLength",audioHeader.getTrackLength());
+
+        return mp3meta;
+    }
+
 }
