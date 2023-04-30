d70480846349cdf27e912f71e9a09b6d373584dd
jm0s <woals1488@naver.com>
2023-04-30 14:14:17 +0900

    db 비밀번호 수정

--- src/main/java/com/tftf/musictaggerserver/MusictaggerserverApplication.java
+++ src/main/java/com/tftf/musictaggerserver/MusictaggerserverApplication.java
@@ -25,6 +25,7 @@ public class MusictaggerserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MusictaggerserverApplication.class, args);
 
+		/*
 		String DATA_DIRECTORY = "C:\\Users\\sskim\\Spring_Projects\\musictaggerserver\\src\\main\\resources\\media\\";
 		File dir = new File(DATA_DIRECTORY);
 		String[] filenames = dir.list((f,name)->name.endsWith(".mp3"));
@@ -140,6 +141,6 @@ public class MusictaggerserverApplication {
 		mp3meta.put("TrackLength",audioHeader.getTrackLength());
 
 		return mp3meta;
+		*/
 	}
-
 }
